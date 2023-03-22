d4d968e227b8a6c9f0de0f690d18df9a1fb7e7d9
zkj <zkjperfecto@gmail.com>
2023-03-22 18:00:55 +0800

    增加drs3算例

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        runSample(20, 4);
+        runSample(20, 3);
     }
     public static void runDefault(int batch_num, int driver_num, int passenger_num) throws Exception{
         Batch batch = new Batch();
--- src/sample/README.md
+++ src/sample/README.md
@@ -1,8 +1,8 @@
-## DynamicRidesharingSample description:
+## DynamicRideSharingSample description:
 $drs1$: 单阶段司机乘客稀疏数据，用于测试
 
 $drs2$:
 
-$drs3$:
+$drs3$: 多阶段司乘od数据，600名司机
 
 $drs4$: 多阶段司乘od数据，乘客数据源自"ds_2023-03-07_all_rush.xlsx"早高峰阶段(07:00 - 09:00)，每隔30秒取样，司机数据随机生成（纬度32.6-32.4，经度118.5-119.2
